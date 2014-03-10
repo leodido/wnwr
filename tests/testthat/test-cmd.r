df52cb44f4eed3ac68711052a0b85ab3f6ca878e
leodido <leodidonato@gmail.com>
2014-03-10 15:59:15 +0100

    Tests for sense filtering

--- tests/testthat/test-cmd.r
+++ tests/testthat/test-cmd.r
@@ -78,5 +78,19 @@ test_that("Supported search options", {
 })
 
 test_that("Errors covering 'sense_num' argument", {
-  
+  expect_that(wn_cmd('word', search_type[1], search_opts[1], letters[1:5L]), throws_error('not a count'))
+  expect_that(wn_cmd('word', search_type[1], search_opts[1], 'abc'), throws_error('not a count'))
+  expect_that(wn_cmd('word', search_type[1], search_opts[1], NA), throws_error('not a count'))
+  expect_that(wn_cmd('word', search_type[1], search_opts[1], 1:10L), throws_error('not a count'))
+  expect_that(wn_cmd('word', search_type[1], search_opts[1], NA_integer_), throws_error('missing value'))
+})
+
+test_that("Sense number filter", {
+  expect_equal(wn_cmd('word', search_type[1]), paste0("wn 'word' -", search_type[1]))
+  expect_equal(wn_cmd('word', search_type[1], search_opts[1]), paste0("wn 'word' -", search_opts[1], " -", search_type[1]))
+  expect_equal(wn_cmd('word', search_type[1], search_opts[1], NULL), paste0("wn 'word' -", search_opts[1], " -", search_type[1]))
+  expect_equal(wn_cmd('word', search_type[1], search_opts[1], c()), paste0("wn 'word' -", search_opts[1], " -", search_type[1]))
+  expect_equal(wn_cmd('word', search_type[1], sense_num = 10L), paste0("wn 'word' -n", 10L, " -", search_type[1]))
+  expect_equal(wn_cmd('word', search_type[1], sense_num = 1), paste0("wn 'word' -n", 1, " -", search_type[1]))
+  expect_equal(wn_cmd('word', search_type[1], search_opts[1], c(1)), paste0("wn 'word' -", search_opts[1], " -n", 1, " -", search_type[1]))
 })
